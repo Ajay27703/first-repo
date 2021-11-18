4cfa707e6f92c90a72849fdf551bd76c40612c27
Ajay <ajay27703@gmail.com>
2021-11-18 17:15:45 +0530

    okie

---
+++ Assignment2/qes1.js
@@ -0,0 +1,24 @@
+// write a program to check if a number is divisible by 7 or not ?
+
+
+// using ternary operator
+var num=13;
+num % 7 ==0? console.log("yes"): console.log("No");
+
+// using ifelse statement (doubt)
+let numb=13
+if (numb%7==0){
+    console.log("yes");
+}
+else{
+    console.log("no");
+}
+
+
+let number=14
+if (number%7==0){
+    console.log("yes");
+}
+else{
+    console.log("no");
+}
